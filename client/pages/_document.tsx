3cebc6e2a0e1f0d73414889d4e2e19fe1e7a484f
HyeongJin Kim <khj7741@gmail.com>
2023-01-06 18:05:23 +0900

    project 초기 세팅

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -1,5 +1,5 @@
+import React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import { ServerStyleSheet } from "styled-components";
 import { ServerStyleSheets } from "@mui/styles";
 
 export default class MyDocument extends Document {
@@ -17,28 +17,18 @@ export default class MyDocument extends Document {
 }
 
 MyDocument.getInitialProps = async (ctx) => {
-  const sheet = new ServerStyleSheet();
   const materialSheets = new ServerStyleSheets();
   const originalRenderPage = ctx.renderPage;
 
-  try {
-    ctx.renderPage = () =>
-      originalRenderPage({
-        enhanceApp: (App) => (props) =>
-          sheet.collectStyles(materialSheets.collect(<App {...props} />)),
-      });
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) =>
+        materialSheets.collect(<App {...props} />),
+    });
 
-    const initialProps = await Document.getInitialProps(ctx);
-    return {
-      ...initialProps,
-      styles: (
-        <>
-          {initialProps.styles}
-          {sheet.getStyleElement()}
-        </>
-      ),
-    };
-  } finally {
-    sheet.seal();
-  }
+  const initialProps = await Document.getInitialProps(ctx);
+  return {
+    ...initialProps,
+    styles: <>{initialProps.styles}</>,
+  };
 };
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,94 +1,11 @@
-import axios from "axios";
-import Link from "next/link";
+import { Button } from "@mui/material";
 import { Fragment } from "react";
 
 export default function Home() {
-  const Token = `asdfjklasdf`;
-  const URL = "http://amazone";
-  async function getRequest() {
-    try {
-      const res = await axios({
-        method: "get",
-        url: `${URL}`,
-        headers: { Authorization: `Bearer ${Token}` },
-      });
-      console.log(res);
-    } catch (e) {
-      console.log(e);
-    }
-  }
-  async function postRequest() {
-    try {
-      const res = await axios({
-        method: "post",
-        url: `${URL}`,
-        data: { id: "human001", password: "asdfqwer1234" },
-        headers: { Authorization: `Bearer ${Token}` },
-      });
-      console.log(res);
-    } catch (e) {
-      console.log(e);
-    }
-  }
-  async function putRequest() {
-    try {
-      const res = await axios({
-        method: "put",
-        url: `${URL}`,
-        data: { id: "human001", password: "asdfqwer1234" },
-        headers: { Authorization: `Bearer ${Token}` },
-      });
-      console.log(res);
-    } catch (e) {
-      console.log(e);
-    }
-  }
-  async function patchRequest() {
-    try {
-      const res = await axios({
-        method: "patch",
-        url: `${URL}`,
-        data: { id: "human001", password: "asdfqwer1234" },
-        headers: { Authorization: `Bearer ${Token}` },
-      });
-      console.log(res);
-    } catch (e) {
-      console.log(e);
-    }
-  }
-  async function deleteRequest() {
-    try {
-      const res = await axios({
-        method: "delete",
-        url: `${URL}`,
-        data: { id: "human001", password: "asdfqwer1234" },
-        headers: { Authorization: `Bearer ${Token}` },
-      });
-      console.log(res);
-    } catch (e) {
-      console.log(e);
-    }
-  }
   return (
     <Fragment>
-      <Link href="/login">Login page</Link>
-
-      <Link href="/signup">Sign up page</Link>
-      <div>
-        <button onClick={getRequest}>get</button>
-      </div>
-      <div>
-        <button onClick={postRequest}>post</button>
-      </div>
-      <div>
-        <button onClick={putRequest}>put</button>
-      </div>
-      <div>
-        <button onClick={patchRequest}>patch</button>
-      </div>
-      <div>
-        <button onClick={deleteRequest}>delete</button>
-      </div>
+      <Button>테스트</Button>
+      <h1 className="text-3xl font-bold underline">Hello world</h1>
     </Fragment>
   );
 }
