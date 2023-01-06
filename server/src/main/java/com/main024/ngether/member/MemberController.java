2e8d7b46b81fc228282836743bc61ec630009038
Aru-slave <chlwlgus9584@naver.com>
2023-01-06 11:43:48 +0900

    #2 좋아요 api 재수정

--- server/src/main/java/com/main024/ngether/member/MemberController.java
+++ server/src/main/java/com/main024/ngether/member/MemberController.java
@@ -1,5 +1,6 @@
 package com.main024.ngether.member;
 
+import com.main024.ngether.board.BoardMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -14,9 +15,12 @@ public class MemberController {
     private final MemberService memberService;
     private final MemberMapper mapper;
 
-    public MemberController(MemberService memberService, MemberMapper mapper) {
+    private final BoardMapper boardMapper;
+
+    public MemberController(MemberService memberService, MemberMapper mapper, BoardMapper boardMapper) {
         this.memberService = memberService;
         this.mapper = mapper;
+        this.boardMapper = boardMapper;
     }
 
     //회원가입
@@ -70,4 +74,10 @@ public class MemberController {
     public ResponseEntity search(@RequestParam(value = "keyword") String keyword) {
         return ResponseEntity.ok(mapper.memberToMemberResponse(memberService.findByNiceName(keyword)));
     }
+    //내가 좋아요 누른 게시물 검색
+    @GetMapping("/like")
+    public ResponseEntity searchMyLike() {
+        return ResponseEntity.ok(boardMapper.boardsToBoardResponses(memberService.findMyLike()));
+    }
+
 }
