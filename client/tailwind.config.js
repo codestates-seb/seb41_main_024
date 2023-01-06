b3da19ca913ee5c81b7544c494726b9bd01aa319
SHWsgithub <shw961016@gmail.com>
2023-01-06 23:41:22 +0900

    chore - MUI, tailwind css config 설정

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  corePlugins: {
+    preflight: false,
+  },
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
