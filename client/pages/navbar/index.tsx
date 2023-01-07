995e988bdd2f3ed8b926fc1f9ec76b04cbba2bfe
“Pikadev1771” <pikadev1771@gmail.com>
2023-01-08 01:39:51 +0900

    feat - #26 navbar button & navbar 생성

---
+++ client/pages/navbar/index.tsx
@@ -0,0 +1,13 @@
+import { Fragment } from 'react';
+import Navbar from '../../components/organism/navbar/Navbar';
+
+//temp
+const NavbarPage = () => {
+  return (
+    <div className="m-10">
+      <Navbar />
+    </div>
+  );
+};
+
+export default NavbarPage;
