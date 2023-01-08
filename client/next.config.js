edaf4849dc43e613bd4d47a700d46c8b8a199311
“Pikadev1771” <pikadev1771@gmail.com>
2023-01-08 19:11:31 +0900

    chore - next.config.js 수정

--- client/next.config.js
+++ client/next.config.js
@@ -1,30 +1,12 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   webpack: (config) => {
-//     config.module.rules.push({
-//       test: /\.svg$/i,
-//       issuer: /\.[jt]sx?$/,
-//       use: ['@svgr/webpack', 'url-loader'],
-//     });
-//     return config;
-//   },
-// };
-// module.exports = nextConfig;
-
-//next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  // Next 11버전부터는 Webpack config를 next.config에 묶어서
-  // 사용할수 있다고 한다.
-  webpack: {
-    resolve: {
-      fallback: {
-        util: require.resolve('util/'),
-      },
-    },
+  webpack(config, { dev, webpack }) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack', 'url-loader'],
+    });
+    return config;
   },
 };
 
