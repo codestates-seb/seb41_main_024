991c3e99ac6be309dbb34d5a904f4e69383fd470
Aru-slave <chlwlgus9584@naver.com>
2023-01-09 11:15:04 +0900

    feat - #22 채팅 테스트완료

--- server/src/main/java/com/main024/ngether/config/ChatConfig.java
+++ server/src/main/java/com/main024/ngether/config/ChatConfig.java
@@ -12,18 +12,14 @@ public class ChatConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        // stomp 접속 주소 엔드포인트 => /ngetherChat
-        registry.addEndpoint("/chat") // 연결될 엔드포인트
-                .setAllowedOrigins("*")
-                .withSockJS(); // SocketJS 를 연결한다는 설정
+        registry.addEndpoint("/ws/chat").setAllowedOriginPatterns("*").withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        // 메시지를 구독하는 요청 url => 즉 메시지 받을 때
-        registry.enableSimpleBroker("/sub");
 
-        // 메시지를 발행하는 요청 url => 즉 메시지 보낼 때
-        registry.setApplicationDestinationPrefixes("/pub");
+        registry.enableSimpleBroker("/queue", "/topic");
+
+        registry.setApplicationDestinationPrefixes("/app");
     }
 }
