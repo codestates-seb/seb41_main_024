b28b53c83ab10bc150663c46239c1fc2afbf05a2
Aru-slave <chlwlgus9584@naver.com>
2023-01-09 10:05:44 +0900

    feat - #22 채팅 테스트

---
+++ server/src/main/java/com/main024/ngether/chat/ChatRoom.java
@@ -0,0 +1,24 @@
+package com.main024.ngether.chat;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ChatRoom {
+
+    private String roomId;
+    private String roomName;
+
+
+    public static ChatRoom create(String name) {
+        ChatRoom room = new ChatRoom();
+        room.roomId = UUID.randomUUID().toString();
+        room.roomName = name;
+        return room;
+    }
+}
---
+++ server/src/main/java/com/main024/ngether/config/ChatConfig.java
@@ -0,0 +1,29 @@
+package com.main024.ngether.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class ChatConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // stomp 접속 주소 엔드포인트 => /ngetherChat
+        registry.addEndpoint("/chat") // 연결될 엔드포인트
+                .setAllowedOrigins("*")
+                .withSockJS(); // SocketJS 를 연결한다는 설정
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        // 메시지를 구독하는 요청 url => 즉 메시지 받을 때
+        registry.enableSimpleBroker("/sub");
+
+        // 메시지를 발행하는 요청 url => 즉 메시지 보낼 때
+        registry.setApplicationDestinationPrefixes("/pub");
+    }
+}
