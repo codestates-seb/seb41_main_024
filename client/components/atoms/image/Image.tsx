6e046a166a7a21e95aed3fdce060987620f0e525
HyeongJin Kim <khj7741@gmail.com>
2023-01-10 10:34:28 +0900

    style - #24 Next/image 컨테이너 추가

--- client/components/atoms/image/Image.tsx
+++ client/components/atoms/image/Image.tsx
@@ -1,8 +1,15 @@
+import Image from 'next/image';
 import React from 'react';
+import styles from './image.module.css';
+
 import { imagePropsType } from './Type_Image';
 
 const Img = ({ src, alt }: imagePropsType) => {
-  return <img src={src} alt={alt} />;
+  return (
+    <div className={`relative ${styles.imgContainer}`}>
+      <Image src={src} alt={alt} fill />
+    </div>
+  );
 };
 
 export default Img;
