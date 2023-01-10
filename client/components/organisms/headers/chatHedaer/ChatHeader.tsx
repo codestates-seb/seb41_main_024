c117cbdf4c9ef29b5a980895132a5421a3eb2b29
HyeongJin Kim <khj7741@gmail.com>
2023-01-10 11:16:36 +0900

    chore - single quote

--- client/components/organisms/headers/chatHedaer/ChatHeader.tsx
+++ client/components/organisms/headers/chatHedaer/ChatHeader.tsx
@@ -10,25 +10,25 @@ const ChatHeader = () => {
   return (
     <>
       <AppBar
-        className="px-4 py-4 border-b border-b-inherit"
-        position="static"
-        color="inherit"
+        className='px-4 py-4 border-b border-b-inherit'
+        position='static'
+        color='inherit'
         elevation={0}
         sx={{ height: '50px' }}
       >
-        <div className="flex items-center justify-center">
+        <div className='flex items-center justify-center'>
           <button
-            className="border-0 bg-inherit"
-            type="button"
+            className='border-0 bg-inherit'
+            type='button'
             onClick={() => router.back()}
           >
-            <img src={arrowback} alt="뒤로가기" />
+            <img src={arrowback} alt='뒤로가기' />
           </button>
-          <div className="flex flex-1 justify-center">
-            <img src={logo} alt="로고" />
+          <div className='flex flex-1 justify-center'>
+            <img src={logo} alt='로고' />
           </div>
-          <button className="border-0 bg-inherit" type="button">
-            <img src={menu} alt="메뉴열기" />
+          <button className='border-0 bg-inherit' type='button'>
+            <img src={menu} alt='메뉴열기' />
           </button>
         </div>
       </AppBar>
--- client/components/organisms/headers/mainHeader/MainHeader.tsx
+++ client/components/organisms/headers/mainHeader/MainHeader.tsx
@@ -8,23 +8,23 @@ const MainHeader = () => {
   return (
     <>
       <AppBar
-        className="px-4 py-4 border-b border-b-inherit"
-        position="static"
-        color="inherit"
+        className='px-4 py-4 border-b border-b-inherit'
+        position='static'
+        color='inherit'
         elevation={0}
         sx={{ height: '50px' }}
       >
-        <div className="flex">
-          <div className="flex-1">
-            <Link href="/">
-              <img src={logo} alt="메인로고" />
+        <div className='flex'>
+          <div className='flex-1'>
+            <Link href='/'>
+              <img src={logo} alt='메인로고' />
             </Link>
           </div>
-          <button className="border-0 bg-inherit mr-4" type="button">
-            <img src={search} alt="검색하기" />
+          <button className='border-0 bg-inherit mr-4' type='button'>
+            <img src={search} alt='검색하기' />
           </button>
-          <button className="border-0 bg-inherit ml-4" type="button">
-            <img src={navigator} alt="메뉴열기" />
+          <button className='border-0 bg-inherit ml-4' type='button'>
+            <img src={navigator} alt='메뉴열기' />
           </button>
         </div>
       </AppBar>
