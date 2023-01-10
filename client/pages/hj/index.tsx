c08173fb23b108091a8c8091ed3b7bf18b233b93
HyeongJin Kim <khj7741@gmail.com>
2023-01-10 18:17:15 +0900

    page

---
+++ client/pages/hj/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Footer from '../../components/molecules/footer/Footer';
+import Slogan from '../../components/molecules/slogan/Slogan';
+
+const Index = () => {
+  return (
+    <>
+      <Slogan />
+      <Footer />
+    </>
+  );
+};
+
+export default Index;
