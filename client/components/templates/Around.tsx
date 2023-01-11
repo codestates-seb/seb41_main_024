5bc35da4094dd5eb94c928467e3ae36700594803
HyeongJin Kim <khj7741@gmail.com>
2023-01-11 11:41:32 +0900

    style - #54 margin left,right 추가

--- client/components/templates/Around.tsx
+++ client/components/templates/Around.tsx
@@ -6,12 +6,13 @@ import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
 import styles from './around.module.css';
 import AroundList from '../organisms/aroundList/AroundList';
 import Navbar from '../organisms/navbar/Navbar';
+import Img from '../atoms/image/Image';
 const Around = (props) => {
   return (
     <div className="flex flex-col items-center">
       <MainHeader />
-      <div className="mx-auto">
-        <Map className={styles.image} />
+      <div className="mx-auto w-full h-fit">
+        <Img src="/sharingList/map.svg" alt="지도" />
       </div>
       <MenuOutlinedIcon />
       <AroundList />
