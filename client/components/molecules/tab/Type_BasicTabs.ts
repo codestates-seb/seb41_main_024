d6114fa8a0a4309884f3894617c9cd5a6450b2dd
HyeongJin Kim <khj7741@gmail.com>
2023-01-11 12:46:30 +0900

    style - #53 tab props type

--- client/components/molecules/tab/Type_BasicTabs.ts
+++ client/components/molecules/tab/Type_BasicTabs.ts
@@ -1,5 +1,12 @@
 import React from 'react';
 export interface BasicTabsPropsType {
-  handleChange: React.SyntheticEvent<Element, Event>;
+  handleChange: (
+    event: React.SyntheticEvent<Element, Event>,
+    value: any
+  ) => void;
   value: string;
+  bgcolor: string;
+  color: string;
+  borderBottom: number;
+  borderColor: string;
 }
