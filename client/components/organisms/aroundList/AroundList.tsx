99e0b219f3b32b7856079ea3070d225f4c747b84
HyeongJin Kim <khj7741@gmail.com>
2023-01-11 11:30:42 +0900

    style - #54 주변목록 리스트 grid, 더미데이터

---
+++ client/components/organisms/aroundList/AroundList.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import SharingListItem from '../../molecules/sharingListItem/SharingListItem';
+const dummyData = [
+  {
+    src: '/sharingList/pepsi.svg',
+    alt: '펩시콜라',
+    title:
+      '펩시콜라 200캔 소분하실 분 모집합니다 이틀 동안만 모집해요~~ 얼른 와주세요',
+    isFavorite: true,
+  },
+  {
+    src: '/sharingList/pepsi.svg',
+    alt: '펩시콜라',
+    title:
+      '펩시콜라 200캔 소분하실 분 모집합니다 이틀 동안만 모집해요~~ 얼른 와주세요',
+    isFavorite: true,
+  },
+  {
+    src: '/sharingList/pepsi.svg',
+    alt: '펩시콜라',
+    title:
+      '펩시콜라 200캔 소분하실 분 모집합니다 이틀 동안만 모집해요~~ 얼른 와주세요',
+    isFavorite: true,
+  },
+  {
+    src: '/sharingList/pepsi.svg',
+    alt: '펩시콜라',
+    title:
+      '펩시콜라 200캔 소분하실 분 모집합니다 이틀 동안만 모집해요~~ 얼른 와주세요',
+    isFavorite: true,
+  },
+  {
+    src: '/sharingList/pepsi.svg',
+    alt: '펩시콜라',
+    title:
+      '펩시콜라 200캔 소분하실 분 모집합니다 이틀 동안만 모집해요~~ 얼른 와주세요',
+    isFavorite: true,
+  },
+  {
+    src: '/sharingList/pepsi.svg',
+    alt: '펩시콜라',
+    title:
+      '펩시콜라 200캔 소분하실 분 모집합니다 이틀 동안만 모집해요~~ 얼른 와주세요',
+    isFavorite: true,
+  },
+];
+const AroundList = () => {
+  return (
+    <div className="grid grid-cols-2 gap-4 my-5">
+      {dummyData.map((sharingItem) => (
+        <SharingListItem
+          src={sharingItem.src}
+          alt={sharingItem.alt}
+          title={sharingItem.title}
+          isFavorite={sharingItem.isFavorite}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default AroundList;
