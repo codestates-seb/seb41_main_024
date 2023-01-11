ec5386d57446f4c0556a3cfb5671775b85d585f3
HyeongJin Kim <khj7741@gmail.com>
2023-01-11 11:41:09 +0900

    style - #54 margin left,right 추가

--- client/components/organisms/aroundList/AroundList.tsx
+++ client/components/organisms/aroundList/AroundList.tsx
@@ -46,7 +46,7 @@ const dummyData = [
 ];
 const AroundList = () => {
   return (
-    <div className="grid grid-cols-2 gap-4 my-5">
+    <div className="grid grid-cols-2 gap-4 m-5 w-fit">
       {dummyData.map((sharingItem) => (
         <SharingListItem
           src={sharingItem.src}
