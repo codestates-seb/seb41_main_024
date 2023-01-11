cc56451ab55e2320203d206cd30a874acc0b1b2d
koreadinosaur <khj7741@gmail.com>
2023-01-11 15:21:32 +0900

    refactor - #53 폴더구조 변경

--- client/components/organisms/tab/myPageTab/MyPageTab.tsx
+++ client/components/organisms/tab/myPageTab/MyPageTab.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
-import TabPanel from '../../atoms/tabPanel/TabPanel';
-import SharingListItem from '../../molecules/sharingListItem/SharingListItem';
-import BasicTabs from '../../molecules/tab/BasicTabs';
+import TabPanel from '../../../atoms/tabPanel/TabPanel';
+import SharingListItem from '../../../molecules/sharingListItem/SharingListItem';
+import BasicTabs from '../../../molecules/tab/BasicTabs';
+const dummyLabel = [
+  { label: '내정보', index: 0 },
+  { label: '쉐어링', index: 1 },
+  { label: '1:1 문의', index: 2 },
+];
 
 const MyPageTab = () => {
   const [value, setValue] = React.useState(0);
@@ -11,7 +16,12 @@ const MyPageTab = () => {
   };
   return (
     <>
-      <BasicTabs value={value} handleChange={handleChange} />
+      <BasicTabs
+        value={value}
+        handleChange={handleChange}
+        tabLabels={dummyLabel}
+        centered={true}
+      />
       <TabPanel value={value} index={0}>
         <SharingListItem
           title="test"
