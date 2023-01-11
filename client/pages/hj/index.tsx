866861e61d6da112df57a9fae517b009b333f512
HyeongJin Kim <khj7741@gmail.com>
2023-01-11 10:35:56 +0900

    chore - 일부 설정변경

--- client/pages/hj/index.tsx
+++ client/pages/hj/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import Footer from '../../components/molecules/footer/Footer';
+import Tab from '../../components/molecules/tab/BasicTabs';
+import MyPageTab from '../../components/organisms/tab/MyPageTab';
 
 const index = () => {
   return (
     <>
+      <MyPageTab />
       <Footer />
     </>
   );
