2323efec002123336c521723a50cb4a456648367
Park Kyung Hyun <pikadev1771@gmail.com>
2023-01-12 20:21:23 +0900

    feat - #57 AddNew Page UI

---
+++ client/pages/addnew/index.tsx
@@ -0,0 +1,139 @@
+import MainHeader from '../../components/organisms/headers/mainHeader/MainHeader';
+import BottomNav from '../../components/organisms/bottomNav/bottomNav';
+import Footer from '../../components/molecules/footer/Footer';
+import Input from '../../components/atoms/input/Input';
+import FormButton from '../../components/atoms/formbutton/FormButton';
+import Label from '../../components/atoms/label/Label';
+import TextField from '../../components/molecules/passwordTextField/TextField';
+import { useState } from 'react';
+import { ReactComponent as Logo } from '../../public/logos/logoRow.svg';
+import Box from '@mui/material /Box';
+import Stack from '@mui/material/Stack';
+import CameraAltOutlinedIcon from '@mui/icons-material/CameraAltOutlined';
+import Image from 'next/image';
+import Container from '@mui/material/Container';
+import base from '../../public/imageBox/base-box.svg';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import { FormControl, FormHelperText } from '@mui/material';
+
+const AddNewPage = () => {
+  const [form, setForm] = useState({
+    productName: '',
+    price: '',
+    url: '',
+    category: '상품 쉐어링',
+    quantity: 1,
+    spot: '',
+    detail: '',
+  });
+
+  const { productName, price, url, category, quantity, spot, detail } = form;
+
+  const onChange = (
+    event: React.ChangeEvent<HTMLTextAreaElement> | SelectChangeEvent
+  ) => {
+    const { name, value } = event.target;
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
+  return (
+    <div>
+      <MainHeader />
+      <div className="flex justify-center m-7 my-12">
+        <FormControl fullWidth className="flex flex-col w-10/12 max-w-lg">
+          <Stack spacing={4}>
+            <img
+              className="h-40 w-40 mb-7 m-auto"
+              src={base}
+              alt={'유저이미지'}
+            />
+            <Input
+              id="productName"
+              name="productName"
+              type="text"
+              label="상품명"
+              value={productName}
+              onChange={onChange}
+            />
+            <Label htmlFor={'productName'} labelText={''} />
+            <Input
+              id="price"
+              name="price"
+              type="text"
+              label="가격"
+              value={price}
+              onChange={onChange}
+            />
+            <Label htmlFor={'price'} labelText={''} />
+            <Input
+              id="url"
+              name="url"
+              type="text"
+              label="상품 링크"
+              value={url}
+              onChange={onChange}
+            />
+            <Label htmlFor={'url'} labelText={''} />
+            <FormControl fullWidth>
+              <InputLabel id="category">카테고리</InputLabel>
+              <Select
+                labelId="category"
+                id="category-select"
+                value={category}
+                label="category"
+                onChange={onChange}
+              >
+                <MenuItem value="상품 쉐어링">상품 쉐어링</MenuItem>
+                <MenuItem value="배달">배달</MenuItem>
+              </Select>
+            </FormControl>
+            <FormControl fullWidth>
+              <InputLabel id="quantity">상품 개수</InputLabel>
+              <Select
+                labelId="quantity"
+                id="quantity-select"
+                value={quantity}
+                label="quantity"
+                onChange={onChange}
+              >
+                <MenuItem value="1">1</MenuItem>
+                <MenuItem value="2">2</MenuItem>
+              </Select>
+            </FormControl>
+            <Input
+              id="spot"
+              name="spot"
+              type="text"
+              label="쉐어링 위치"
+              value={spot}
+              onChange={onChange}
+            />
+            <Label htmlFor={'spot'} labelText={''} />
+            <Input
+              id="outlined-multiline-flexible"
+              label="내용"
+              rows={10}
+              multiline
+              className="h-15.75"
+            ></Input>
+            <FormButton
+              className="h-14 mt-4"
+              variant="contained"
+              content="쉐어링 등록"
+            />
+          </Stack>
+        </FormControl>
+      </div>
+      <Footer />
+      <BottomNav />
+    </div>
+  );
+};
+
+export default AddNewPage;
