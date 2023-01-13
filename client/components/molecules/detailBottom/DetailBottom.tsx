6bf73ae83137c704496464bcc2ae160f79e92edf
koreadinosaur <khj7741@gmail.com>
2023-01-13 22:20:12 +0900

    refactor - #86 이미지를 버튼 자식요소로 렌더링하기

--- client/components/molecules/detailBottom/DetailBottom.tsx
+++ client/components/molecules/detailBottom/DetailBottom.tsx
@@ -1,23 +1,26 @@
 import FormButton from '../../atoms/formbutton/FormButton';
 import React from 'react';
 import Button from '../../atoms/button/Button';
+import Image from 'next/image';
 
 const DetailBottom = (props) => {
   return (
     <div className="flex justify-between items-center p-4">
-      <Button
-        src="/sharingList/favorite.svg"
-        width={24}
-        height={24}
-        alt="좋아요"
-      />
+      <Button>
+        <Image
+          src="/sharingList/favorite.svg"
+          width={24}
+          height={24}
+          alt="좋아요"
+        />
+      </Button>
       <span>22,000원</span>
       <FormButton
         variant="contained"
         content="신고하기"
         className="bg-[#FF0000]"
-      ></FormButton>
-      <FormButton variant="contained" content="참여하기"></FormButton>
+      />
+      <FormButton variant="contained" content="참여하기" />
     </div>
   );
 };
