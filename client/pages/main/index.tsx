b6c7590740674b9f4f52ad91707e6771624579e5
Park Kyung Hyun <pikadev1771@gmail.com>
2023-01-13 09:03:45 +0900

    style - #57 slogan 수정

--- client/pages/main/index.tsx
+++ client/pages/main/index.tsx
@@ -16,11 +16,11 @@ const MainSlogan = () => {
       <Logo />
       <p className="pt-px mt-4 text-lg">
         <strong className="text-primary font-bold">필요</strong>한 만큼만,
-        <strong className="text-primary font-bold">구매</strong>의 새로운 방법
+        <strong className="text-primary font-bold"> 구매</strong>의 새로운 방법
       </p>
       <p className="pb-px text-lg">
         Find your
-        <strong className="text-primary font-bold"></strong>shopping mate
+        <strong className="text-primary font-bold"> shopping mate</strong>
       </p>
     </div>
   );
