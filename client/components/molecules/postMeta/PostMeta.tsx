9b212e209eb972397e00615b1a4d8a2c0bf8a30d
koreadinosaur <khj7741@gmail.com>
2023-01-14 00:32:08 +0900

    refactor - #86 하드코딩 데이터 props data로 변경

--- client/components/molecules/postMeta/PostMeta.tsx
+++ client/components/molecules/postMeta/PostMeta.tsx
@@ -1,23 +1,31 @@
-const PostMeta = () => {
+import ElapsedTime from '../elapsedTime/ElapsedTime';
+import { postMetaProps } from './postMeta';
+const PostMeta = ({ postData }: postMetaProps) => {
   return (
     <div className="ml-4 mt-6">
       <div className="mb-2.5">
-        <strong className="text-xl font-medium">
-          코멧 개별포장 일자빨대 블랙 500개
+        <strong className="text-xl font-medium">{postData?.title}</strong>
+      </div>
+      <p className="text-sm text-[#475569] mb-4">
+        {postData?.category}•<ElapsedTime createdAt={postData?.create_date} />
+      </p>
+      <div className=" mb-2.5">
+        <strong className="text-lg font-medium">
+          {postData?.price.toLocaleString()}원(배송비포함)
         </strong>
       </div>
-      <p className="text-sm text-[#475569] mb-4">대량주문•25분전</p>
-      <div className=" mb-2.5">
-        <strong className="text-lg font-medium">22,000원(배송비포함)</strong>
-      </div>
-      <p className="text-sm text-[#475569] mb-4">모집기간:~1월 13일</p>
       <p className="text-sm text-[#475569] mb-4">
-        현재 모집인원: 1명 / 총 모집인원: 5명
+        모집기간: ~{postData?.deadLine}
       </p>
-      <p className="text-sm text-[#475569] mb-4">참여시 예상금액: 11,000원</p>
+      <p className="text-sm text-[#475569] mb-4">
+        현재 모집인원: {postData?.curNum}명 / 총 모집인원: {postData?.maxNum}명
+      </p>
+      <p className="text-sm text-[#475569] mb-4">
+        참여시 예상금액:{' '}
+        {(postData?.price / (postData?.curNum + 1)).toLocaleString()}원
+      </p>
       <p className="truncate text-sm text-[#475569] mb-4">
-        판매 제품 링크 :
-        https://www.figma.com/file/c5ndHFggdYDwI79WPIBJQp/Ngether?node-id=51%3A2548&t=FrQ9Bo12zF84j9B4-0
+        판매 제품 링크 : {postData?.productsLink}
       </p>
     </div>
   );
