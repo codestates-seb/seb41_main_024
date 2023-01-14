f6bf24b1c5addfa7eb0aafab295e2cd66b091714
koreadinosaur <khj7741@gmail.com>
2023-01-14 21:00:45 +0900

    style - #92 다중 레이아웃 설정, getLayout 컴포넌트 type 설정

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -3,15 +3,25 @@ import type { AppProps } from 'next/app';
 import { StyledEngineProvider, ThemeProvider } from '@mui/material/styles';
 
 import basicTheme from '../theme/basic';
+import DefaultLayout from '../components/layout/defalutLayout/DefaultLayout';
+import { ReactElement } from 'react';
+import { NextPageWithLayout } from '../components/layout/defalutLayout/defaultLayout';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <StyledEngineProvider injectFirst>
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const renderWithLayout =
+    Component.getLayout ||
+    function (page: ReactElement) {
+      return <DefaultLayout>{page}</DefaultLayout>;
+    };
+  {
+    /* <StyledEngineProvider injectFirst>
       <ThemeProvider theme={basicTheme}>
-        <div className="max-w-2xl min-w-[390px] mx-auto">
-          <Component {...pageProps} />
-        </div>
       </ThemeProvider>
-    </StyledEngineProvider>
-  );
+    </StyledEngineProvider> */
+  }
+  return renderWithLayout(<Component {...pageProps} />);
 }
