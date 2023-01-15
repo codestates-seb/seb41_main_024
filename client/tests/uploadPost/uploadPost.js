c6d5d5d4ffa24b300f5d9a37986811f6e3bf2271
HyeongJin Kim <khj7741@gmail.com>
2023-01-15 23:26:10 +0900

    chore - #104 jest 기본 설정

---
+++ client/tests/uploadPost/uploadPost.js
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react';
+import MainPage from '../../pages/index';
+
+describe('Home', () => {
+  it('renders a heading', () => {
+    const { container } = render(<MainPage />);
+
+    const heading = screen.getByRole('heading', {
+      name: /welcome to next\.js!/i,
+    });
+
+    expect(heading).toBeInTheDocument();
+
+    expect(container).toMatchSnapshot();
+  });
+});
