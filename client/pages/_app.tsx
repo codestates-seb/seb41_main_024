fa1a490e6c15fb97671f3c7079fe779d73081225
koreadinosaur <khj7741@gmail.com>
2023-01-16 22:55:01 +0900

    chore - #104 react query 기본 설정

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,8 +1,6 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { StyledEngineProvider, ThemeProvider } from '@mui/material/styles';
-
-import basicTheme from '../theme/basic';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import DefaultLayout from '../components/layout/defalutLayout/DefaultLayout';
 import { ReactElement } from 'react';
 import { NextPageWithLayout } from '../components/layout/defalutLayout/defaultLayout';
@@ -17,11 +15,10 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
     function (page: ReactElement) {
       return <DefaultLayout>{page}</DefaultLayout>;
     };
-  {
-    /* <StyledEngineProvider injectFirst>
-      <ThemeProvider theme={basicTheme}>
-      </ThemeProvider>
-    </StyledEngineProvider> */
-  }
-  return renderWithLayout(<Component {...pageProps} />);
+  const queryClient = new QueryClient();
+  return renderWithLayout(
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
