5523257ca4cdcff0d8ee7fcc5f2f38c1a63bd680
HyeongJin Kim <khj7741@gmail.com>
2023-01-17 18:22:46 +0900

    feat - #134 useQuery  추가

--- client/pages/nearby/index.tsx
+++ client/pages/nearby/index.tsx
@@ -2,8 +2,17 @@ import { BottomNavigation } from '@mui/material';
 import React from 'react';
 import Img from '../../components/atoms/image/Image';
 import NearByPageTab from '../../components/organisms/tab/nearByPageTab/NearByPageTab';
+import { useQuery } from '@tanstack/react-query';
+import { getPosts } from '../../api/post';
 
 const Index = () => {
+  const locationId = 1;
+  const range = 1;
+  const category = '상품 쉐어링';
+  const { isLoading, isError, data, error } = useQuery({
+    queryKey: ['sharingList'],
+    queryFn: () => getPosts({ locationId, range, category }),
+  });
   return (
     <div className="flex flex-col items-center">
       <div className="mx-auto w-full h-fit">
