244ea4ee95252de2826b70b553d5ffac351f1c54
Aru-slave <chlwlgus9584@naver.com>
2023-01-17 18:08:02 +0900

    feat - #64 chatHandler수정

--- server/src/main/java/com/main024/ngether/config/CorsConfig.java
+++ server/src/main/java/com/main024/ngether/config/CorsConfig.java
@@ -1,10 +1,12 @@
 package com.main024.ngether.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+@Configuration
 public class CorsConfig {
 
     @Bean
