6976fd5b09f97f1b0264734ffc85d5a40eb4efa2
Aru-slave <chlwlgus9584@naver.com>
2023-01-17 18:51:15 +0900

    feat - #64 fig 수정

--- server/src/main/java/com/main024/ngether/chat/chatController/MessageController.java
+++ server/src/main/java/com/main024/ngether/chat/chatController/MessageController.java
@@ -18,6 +18,7 @@ import java.time.LocalDateTime;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin
 public class MessageController {
 
 
--- server/src/main/java/com/main024/ngether/config/SecurityConfiguration.java
+++ server/src/main/java/com/main024/ngether/config/SecurityConfiguration.java
@@ -8,7 +8,6 @@ import com.main024.ngether.auth.handler.MemberAuthenticationSuccessHandler;
 import com.main024.ngether.auth.jwt.JwtTokenizer;
 import com.main024.ngether.auth.utils.CustomAuthorityUtils;
 import com.main024.ngether.location.LocationRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -18,17 +17,13 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 @Configuration
-@CrossOrigin
 public class SecurityConfiguration {
     private final JwtTokenizer jwtTokenizer;
     private final CustomAuthorityUtils authorityUtils;
