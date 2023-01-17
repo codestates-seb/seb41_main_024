2ea541db11d813a7c2be5852aca5a79612e6ae04
HyeongJin Kim <khj7741@gmail.com>
2023-01-17 17:06:58 +0900

    feat - #134 거리별 api요청

--- client/api/post.ts
+++ client/api/post.ts
@@ -8,13 +8,22 @@ interface uploadPostType {
   maxNum: string;
   address: string;
   content: string;
-
   latitude: string;
   longitude: string;
   deadLine: string;
   accessToken: string;
   refreshToken: string;
 }
+interface getPostType {
+  token: {
+    accessToken: string;
+    refreshToken: string;
+  };
+  locationId: string;
+  range: number;
+  category: string;
+}
+const REQUEST_URL = 'http://3.34.54.131:8080';
 
 export const uploadPost = async (data: uploadPostType) => {
   const query = data.category === '배달' ? 'delivery' : 'product';
@@ -22,8 +31,16 @@ export const uploadPost = async (data: uploadPostType) => {
     method: 'post',
     data,
     headers: { Authorization: data.accessToken, Refresh: data.refreshToken },
-    url: `http://3.34.54.131:8080/api/boards?category=${query}`,
+    url: `${REQUEST_URL}/api/boards?category=${query}`,
   });
+  return response.data;
+};
 
+export const getPosts = async ({ token, locationId, range, category }) => {
+  const { data: response } = await axios({
+    method: 'get',
+    headers: { Authorization: token.accessToken, Refresh: token.refreshToken },
+    url: `${REQUEST_URL}/api/distance/${locationId}range=${range}&&category=${category}`,
+  });
   return response.data;
 };
