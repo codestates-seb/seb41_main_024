4e1d21269a5df90eae9e13bfe8997d4df93e767c
Aru-slave <chlwlgus9584@naver.com>
2023-01-19 11:41:18 +0900

    feat - #64 chatController 수정

--- server/src/main/java/com/main024/ngether/chat/chatController/ChatRoomController.java
+++ server/src/main/java/com/main024/ngether/chat/chatController/ChatRoomController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequiredArgsConstructor//자동으로 생성자 주입 해줌
 @RequestMapping("/chat")
 public class ChatRoomController {
