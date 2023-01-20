1af7d61c3db61a2cb5661510946f8efbb5966270
SHWsgithub <shw961016@gmail.com>
2023-01-20 16:30:09 +0900

    refactor: 메세지를 받아올 때 시간을 바꿔야되는 중복적인 부분transDateFormChatMessage 함수로 처리

--- client/hooks/useWebSocketClient.ts
+++ client/hooks/useWebSocketClient.ts
@@ -20,54 +20,40 @@ const useWebSocketClient = (token: {Authorization : string | undefined}) => {
   const [stompClient, setStompClient] = useState<StompJS.Client | null>(null);
 
   useEffect( () => {
-    // 쿠키가 왜 언디파인드일지 파악하보고
-    // 단순 로컬 오류였음
-    // 셋타임아웃으로 초단위를 맞추는건 지양하기
       if(!isReady && token !== undefined) return
-      // 이부분이 동기로 작동하는데 비동기로 짜보는게 좋겠다
-      // 
-      const sockjs = new SockJS(`https://ngether.site/ws`);
-      const ws = StompJS.over(sockjs);
-      setStompClient(ws);
 
       const setChatWebsocket = async () => {
-        await axios.get(`https://ngether.site/chat/room/messages/${roomId}`, {headers : token})
-        .then(res => setMessages(res.data.map((chatMessage: chatMessageType) => {
-          console.log(res)
-          return { ...chatMessage, createDate: transDateFormat(chatMessage.createDate) };
-        })));
-        
-        // .then(res => setMessages(res.data.map((chatMessage: chatMessageType) => {
-        //   console.log(res)
-        //   return { ...chatMessage, createDate: transDateFormat(chatMessage.createDate) };
-        // })));
-        // 중복되는 부분은 함수로 관리하면 좋겠다.
-        // function createChatMessage = (chatMessage: chatMessageType) => {
-        // return {...chatMessage, createDate: transDateFormat(chatMessage.createDate)}
-        
-        await ws.connect(
-          {},
-          () => {
-            ws.subscribe(
-            `/receive/chat/${roomId}`, 
-            (messages) => {
-              console.log(messages)
-              let parsedMessage = JSON.parse(messages.body);
-              parsedMessage.createDate = transDateFormat(parsedMessage.createDate)
-              setMessages((prev) => [...prev, parsedMessage])
+        try {
+          const sockjs = new SockJS(`https://ngether.site/ws`);
+          const ws = StompJS.over(sockjs);
+          setStompClient(ws);
+  
+          await axios.get(`https://ngether.site/chat/room/messages/${roomId}`, {headers : token})
+          .then(res => setMessages(res.data.map(transDateFormChatMessage)));
+          
+          await ws.connect(
+            {token},
+            () => {
+              ws.subscribe(
+              `/receive/chat/${roomId}`, 
+              (messages) => {
+                setMessages((prevMessages) => [...prevMessages, transDateFormChatMessage(JSON.parse(messages.body))])
+              }, 
+              {token});
+              axios.get(`https://ngether.site/chat/room/enter/${roomId}`, {headers: token})
+              .then(res => setMembers(res.data.map((member: { memberId: number, nickName: string; }) => member.nickName)));
             }, 
-            {});
-            axios.get(`https://ngether.site/chat/room/enter/${roomId}`, {headers: token})
-            .then(res => setMembers(res.data.map((member: { memberId: number, nickName: string; }) => member.nickName)));
-          }, 
-          (error) => {
-            console.log(error)
-          }
-        )
+            (error) => {
+              console.log(error)
+            }
+          )
+        }
+        catch (error) {
+          console.log(error)
+        }
       }
       setChatWebsocket();
   }, [roomId, token])
-
   return {stompClient, messages, members, roomId}
 }
 
@@ -77,6 +63,16 @@ const transDateFormat = (date: string) => {
   const targetDate = new Date(date);
   const formattedDate = `${targetDate.getHours() >= 12 ? '오후' : '오전'} ${targetDate.getHours() % 12 || 12}시 ${targetDate.getMinutes()}분`;
   return formattedDate
+}
+
+// 지속적으로 반복되는 부분을 수정
+// 32번째 줄 map 내부, 40번째 줄에서도 파싱이 필요한 상황이었지만 
+// transDateFormChatMessage을 통해 해결
+// let parsedMessage = JSON.parse(messages.body);
+// parsedMessage.createDate = transDateFormat(parsedMessage.createDate);
+// 이 부분도 parse부분을 인자로 직접 넣으면 됨
+const transDateFormChatMessage = (chatMessage: chatMessageType) => {
+  return {...chatMessage, createDate: transDateFormat(chatMessage.createDate)}
 }
 
 // 이거 테스트코드 만들어볼것
