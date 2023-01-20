19320669149ef9aaa5ec741ca0dd978596862c7f
SHWsgithub <shw961016@gmail.com>
2023-01-20 19:05:04 +0900

    feat - #113: 해당 쉐어링 링크 연동 및 css 설정

--- client/pages/chatroom/[roomId].tsx
+++ client/pages/chatroom/[roomId].tsx
@@ -1,10 +1,13 @@
 import ChatGroup from '../../components/organisms/chatGroup/ChatGroup';
-import { ReactElement, useState } from 'react';
+import { Fragment, ReactElement, useEffect, useState } from 'react';
 import ChatForm from '../../components/organisms/chatForm/ChatForm';
 import ChatRoomLayout from '../../components/layout/chatRoomLayout/ChatRoomLayout';
 import useWebSocketClient from '../../hooks/useWebSocketClient';
 import axios from 'axios';
 import Cookies from 'js-cookie';
+import ChatItem from '../../components/organisms/chatItem/ChatItem'
+import { getChatSharing } from '../../api/chatSharing';
+import Link from 'next/link';
 
 // 채팅방 개설시 자동으로 채팅방 개설 및 닉네임 설정
 // 게시물 상세에서 n게더 참여하기 시 게시물 id와 채팅방 id가 똑같습니다.
@@ -16,7 +19,26 @@ let HEADER_TOKEN = {Authorization : Cookies.get('access_token')};
 
 const Chatroom = () => { 
   const [input, setInput] = useState('')
+  const [sharingData, setSharingData] = useState({
+    thumbnail: '',
+    isOpen: false,
+    title: '',
+    price: '',
+    alertNum: '',
+    address: ''
+  })
   const {stompClient, messages, members, roomId} = useWebSocketClient(HEADER_TOKEN);
+
+  useEffect(() => {
+      if(roomId)
+      getChatSharing(roomId)
+      .then((response) => {
+        setSharingData(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [roomId]);
 
   const onChangeInput =  (e: React.ChangeEvent<HTMLTextAreaElement>) => { 
     setInput(e.target.value);
@@ -46,9 +68,21 @@ const Chatroom = () => {
   }
 
   return (
-    <div className="mx-0 mx-auto">
-      <button onClick={exitChatRoom}>퇴장</button>
-      <div className="bg-primary pt-[8.125rem] pb-[7.5rem] min-h-[calc(100vh-121px)]">
+    <div>
+      {/* <button onClick={exitChatRoom}>퇴장</button> */}
+      <div className='left-2/4 translate-x-[-50%] fixed max-w-[672px] w-[80%] px-[3rem] rounded'>
+        <Link href={`/nearby/${roomId}`}>
+          <ChatItem
+            thumbnail={''}
+            isOpen={sharingData.isOpen}
+            title={sharingData.title}
+            price={sharingData.price}
+            address={sharingData.address}
+            alertNum={sharingData.alertNum}
+          />
+        </Link>
+      </div>
+      <div className="bg-primary pt-[90px] pb-[7.5rem] min-h-[calc(100vh-121px)] max-w-[672px] w-[100%]">
         <ChatGroup chatData={messages} />
       </div>
       <div className="fixed bottom-0 left-2/4 translate-x-[-50%] max-w-2xl w-full bg-white">
