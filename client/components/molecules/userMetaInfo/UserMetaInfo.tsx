3fa9e53fcb7a4f3581acbde8a12720d46dc2e15c
Park Kyung Hyun <pikadev1771@gmail.com>
2023-01-21 01:37:02 +0900

    refactor - #123 isWriter에 따른 UI 변경

--- client/components/molecules/userMetaInfo/UserMetaInfo.tsx
+++ client/components/molecules/userMetaInfo/UserMetaInfo.tsx
@@ -1,19 +1,12 @@
-import Image from 'next/image';
 import React from 'react';
 import Button from '../../atoms/button/Button';
 import { productDataProps } from './userMetaInfo';
-import { useCookies } from 'react-cookie';
-import { useRouter } from 'next/router';
-import axios from 'axios';
-import { useQuery, useMutation } from '@tanstack/react-query';
-import Cookies from 'js-cookie';
-import { deleteProductDetail } from '../../../api/detail';
 import Link from 'next/link';
 
 const UserMetaInfo = ({
   productData,
+  handleDelete,
   isWriter,
-  deleteHandler,
   id,
 }: productDataProps) => {
   return (
@@ -27,21 +20,22 @@ const UserMetaInfo = ({
         </strong>
         <p>{productData?.address}</p>
       </div>
-      <div className={isWriter ? 'flex items-center' : 'hidden'}>
-        {/* <div className={`flex items-center ${isWriter ? 'hidden' : ''}`}> */}
-        <Link
-          href={`/edit/${id}`}
-          className="w-14 p-2 m-2 bg-primary text-white rounded inline-block text-center"
-        >
-          수정
-        </Link>
-        <Button
-          className="w-14 p-2 m-2 bg-primary text-white rounded"
-          onClick={deleteHandler}
-        >
-          삭제
-        </Button>
-      </div>
+      {isWriter && (
+        <div className={'flex items-center'}>
+          <Link
+            href={`/edit/${id}`}
+            className="w-14 p-2 m-2 bg-primary text-white rounded inline-block text-center"
+          >
+            수정
+          </Link>
+          <Button
+            className="w-14 p-2 m-2 bg-primary text-white rounded"
+            onClick={handleDelete}
+          >
+            삭제
+          </Button>
+        </div>
+      )}
     </div>
   );
 };
