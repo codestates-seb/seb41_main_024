8190a2b5bb28bcf95b88801d0ba7174608f5373a
koreadinosaur <khj7741@gmail.com>
2023-01-20 11:04:57 +0900

    refactor - Button을 Link로 변경, 비지니스 로직 분리

--- client/components/molecules/userMetaInfo/UserMetaInfo.tsx
+++ client/components/molecules/userMetaInfo/UserMetaInfo.tsx
@@ -8,27 +8,18 @@ import axios from 'axios';
 import { useQuery, useMutation } from '@tanstack/react-query';
 import Cookies from 'js-cookie';
 import { deleteProductDetail } from '../../../api/detail';
+import Link from 'next/link';
 
-const UserMetaInfo = ({ productData }: productDataProps) => {
-  const [cookies, setCookie] = useCookies(['memberId']);
-  const isWriter = Number(cookies.memberId) === productData.memberId;
-  const router = useRouter();
-  const { id } = router.query;
-
-  const handleEdit = () => {
-    router.push(`/edit/${id}`);
-  };
-
-  const deleteMutation = useMutation(() => deleteProductDetail(id));
-
-  console.log(deleteMutation);
-
-  if (deleteMutation.data) {
-    router.push('/');
-  }
+const UserMetaInfo = ({
+  productData,
+  isWriter,
+  deleteHandler,
+  id,
+}: productDataProps) => {
+  console.log('metaInfo', isWriter);
 
   return (
-    <div className="flex items-center border-b-1 border-x-0 border-t-0 border-solid border-[#475569] py-6 px-6">
+    <div className="flex items-center border-b-1 border-x-0 border-t-0 border-solid border-[#475569] py-6 px-6 inline-block">
       <div>
         <div className="bg-emerald-500 w-8 h-8"></div>
       </div>
@@ -38,22 +29,21 @@ const UserMetaInfo = ({ productData }: productDataProps) => {
         </strong>
         <p>{productData?.address}</p>
       </div>
-      {isWriter && (
-        <div>
-          <Button
-            className="w-14 p-2 m-2 bg-primary text-white rounded"
-            onClick={handleEdit}
-          >
-            수정
-          </Button>
-          <Button
-            className="w-14 p-2 m-2 bg-primary text-white rounded"
-            onClick={() => deleteMutation.mutate()}
-          >
-            삭제
-          </Button>
-        </div>
-      )}
+      <div className={isWriter ? 'flex items-center' : 'hidden'}>
+        {/* <div className={`flex items-center ${isWriter ? 'hidden' : ''}`}> */}
+        <Link
+          href={`/edit/${id}`}
+          className="w-14 p-2 m-2 bg-primary text-white rounded inline-block text-center"
+        >
+          수정
+        </Link>
+        <Button
+          className="w-14 p-2 m-2 bg-primary text-white rounded"
+          onClick={deleteHandler}
+        >
+          삭제
+        </Button>
+      </div>
     </div>
   );
 };
