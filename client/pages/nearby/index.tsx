6c05378517b78ad97e5e6bbfbf1c5e9e48e147d7
koreadinosaur <khj7741@gmail.com>
2023-01-23 22:20:08 +0900

    feat - #198 게시물 리스트 getServerSideProps로 SSR 구현하기, 내주변 게시물 marker 표시하기
    refactor - mui 컴포넌트 label과 value를 동일하게 설정하기

--- client/pages/nearby/index.tsx
+++ client/pages/nearby/index.tsx
@@ -2,61 +2,55 @@ import { BottomNavigation } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import Img from '../../components/atoms/image/Image';
 import NearByPageTab from '../../components/organisms/tab/nearByPageTab/NearByPageTab';
-import { useQuery } from '@tanstack/react-query';
-import { getPosts } from '../../api/post';
+import { dehydrate, QueryClient, useQuery } from '@tanstack/react-query';
+import {
+  getPosts,
+  getPostsInSpecifiedLocation,
+  searchPostsByTitle,
+} from '../../api/post';
 import DropdownInput from '../../components/molecules/dropdownInput/DropdownInput';
 import useDropDown from '../../hooks/common/useDropDown';
 import { Map, MapMarker } from 'react-kakao-maps-sdk';
 import { getCurrentLocation } from '../../api/location';
+import { useRouter } from 'next/router';
+import useSearch from '../../hooks/search/useSearch';
+import { setMarkerCluster } from '../../api/kakaoMap';
 const CATEGORY_OPTIONS = [
-  { label: '상품 쉐어링', value: 'product' },
-  { label: '배달음식 쉐어링', value: 'delivery' },
+  { label: '상품 쉐어링', value: '상품 쉐어링' },
+  { label: '배달음식 쉐어링', value: '배달음식 쉐어링' },
 ];
 const DISTANCE_OPTIONS_PRODUCTS = [
-  { label: '0.5km', value: 0.5 },
-  { label: '1km', value: 1 },
-  { label: '1.5km', value: 1.5 },
+  { label: '0.5km', value: '0.5km' },
+  { label: '1km', value: '1km' },
+  { label: '1.5km', value: '1.5km' },
 ];
 const DISTANCE_OPTIONS_DELIVERY = [
-  { label: '200m', value: 0.2 },
-  { label: '400m', value: 0.4 },
-  { label: '600m', value: 0.6 },
+  { label: '200m', value: '200m' },
+  { label: '400m', value: '400m' },
+  { label: '600m', value: '600m' },
 ];
 
-const Index = () => {
-  const locationId = 1;
-  const [location, setLocation] = useState({
-    lat: 35.6194352,
-    lng: 129.3486386,
-  });
+const Index = ({ dehydratedState, lat, lng }) => {
+  const sharingLists = dehydratedState?.queries[0]?.state.data.data;
+
   const { inputValue, onChange } = useDropDown({
     category: '',
     range: '',
   });
-  const [locationError, setLocationError] = useState('');
-  useEffect(() => getCurrentLocation(setLocation, setLocationError), []);
-  console.log(location);
+  useEffect(() => {
+    const coords = { lat, lng };
+    setMarkerCluster(coords, sharingLists);
+  }, []);
 
   const { category, range } = inputValue;
-  const { isLoading, isError, data, error } = useQuery({
-    queryKey: ['sharingList'],
-    queryFn: () => getPosts({ locationId, range, category }),
-    staleTime: 1000 * 60 * 5,
-    refetchOnWindowFocus: true,
-    retry: false,
-  });
-
+  const [locationError, setLocationError] = useState('');
   return (
     <div className="flex flex-col items-center">
       <div className="mx-auto w-full h-fit">
-        <Map
-          center={{ lat: location?.lat, lng: location?.lng }}
-          style={{ width: '100%', height: '360px' }}
-        >
-          <MapMarker position={{ lat: location?.lat, lng: location?.lng }}>
-            <div style={{ color: '#000' }}>Hello World!</div>
-          </MapMarker>
-        </Map>
+        <div id="map" className="w-[100%] h-[350px]"></div>
+        <p>
+          <em>지도를 클릭해주세요!</em>
+        </p>
         {locationError && <div>{locationError}</div>}
       </div>
       <form>
@@ -91,8 +85,62 @@ const Index = () => {
           />
         )}
       </form>
-      <NearByPageTab />
+      <NearByPageTab sharingLists={sharingLists} />
     </div>
   );
 };
 export default Index;
+
+export async function getServerSideProps(context) {
+  const {
+    range: defaultRange,
+    category: defaultCategory,
+    page,
+    size,
+    lat,
+    lng,
+    address,
+    searchOption,
+    type,
+    keyword,
+  } = context?.query;
+  const requestData = {
+    lat: Number(lat),
+    lng: Number(lng),
+    address: decodeURIComponent(address),
+  };
+
+  const argumentOfLocation = {
+    data: requestData,
+    range: defaultRange || '',
+    category: defaultCategory || '',
+    page: page || 1,
+    size: size || 300,
+  };
+  const argumentOfTitle = {
+    type: type || 4,
+    keyword: decodeURIComponent(keyword),
+    page: page || 1,
+    size: size || 300,
+  };
+
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery(['sharingList'], async () => {
+    if (searchOption === '주소') {
+      return await getPostsInSpecifiedLocation(argumentOfLocation);
+    } else if (searchOption === '글 제목') {
+      return await searchPostsByTitle(argumentOfTitle);
+    }
+  });
+  const sharingLists = dehydrate(queryClient);
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient) || '',
+      lat: lat || 37.4954330863648,
+      lng: lng || 126.88750531451,
+      argumentOfLocation: argumentOfLocation || '',
+      argumentOfTitle: argumentOfTitle || '',
+    },
+  };
+}
