05ea5890a3820c27338324e0bf213394645fb28f
koreadinosaur <khj7741@gmail.com>
2023-01-24 00:31:53 +0900

    feat - #198 마커 클러스터, 마커 클릭시 게시물 제목 출력하는 기능

--- client/api/kakaoMap.ts
+++ client/api/kakaoMap.ts
@@ -1,3 +1,5 @@
+import { ListItemPropsType } from '../components/molecules/sharingListItem/sharingListItemType';
+
 interface getMapAndMarkerPropsType {
   center: {
     lat: number;
@@ -101,17 +103,27 @@ export const exchangeCoordToAddress = async (
 
   // 중심 좌표나 확대 수준이 변경됐을 때 지도 중심 좌표에 대한 주소 정보를 표시하도록 이벤트를 등록합니다
 
-  function searchDetailAddrFromCoords(coords, callback) {
+  function searchDetailAddrFromCoords(
+    coords: kakao.maps.LatLng,
+    displayMarkerOnClick: (result: any, status: any) => void
+  ) {
     // 좌표로 법정동 상세 주소 정보를 요청합니다
-    geocoder.coord2Address(coords.getLng(), coords.getLat(), callback);
+    geocoder.coord2Address(
+      coords.getLng(),
+      coords.getLat(),
+      displayMarkerOnClick
+    );
   }
 
   // 지도 좌측상단에 지도 중심좌표에 대한 주소정보를 표출하는 함수입니다
 };
 
-export const setDefaultCoordsAndAddress = (center, callback) => {
+export const setDefaultCoordsAndAddress = (
+  center: getMapAndMarkerPropsType['center'],
+  setCoordsAndAddress: getMapAndMarkerPropsType['setTargetCoord']
+) => {
   let geocoder = new kakao.maps.services.Geocoder();
-  geocoder.coord2Address(center.lng, center.lat, callback);
+  geocoder.coord2Address(center.lng, center.lat, setCoordsAndAddress);
 };
 
 export const searchMap = (searchAddress: string, setCenter: any) => {
@@ -127,4 +139,51 @@ export const searchMap = (searchAddress: string, setCenter: any) => {
     }
   };
   geocoder.addressSearch(`${searchAddress}`, switchLocationToCoordinate);
+};
+
+export const setMarkerCluster = async (
+  coords: getMapAndMarkerPropsType['center'],
+  sharingLists: ListItemPropsType[]
+) => {
+  let mapContainer =
+    document.getElementById('map') || document.createElement('div');
+  const map = new kakao.maps.Map(mapContainer, {
+    // 지도를 표시할 div
+    center: new kakao.maps.LatLng(coords.lat, coords.lng), // 지도의 중심좌표
+    level: 4, // 지도의 확대 레벨
+  });
+  let marker = new kakao.maps.Marker({ position: map.getCenter() }); // 클릭한 위치를 표시할 마커입니다
+  marker.setMap(map);
+  // 마커 클러스터러를 생성합니다
+  const clusterer = new kakao.maps.MarkerClusterer({
+    map: map, // 마커들을 클러스터로 관리하고 표시할 지도 객체
+    averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정
+    minLevel: 4, // 클러스터 할 최소 지도 레벨
+  });
+  console.log(sharingLists);
+  let markers = [];
+
+  for (let i = 0; i < sharingLists?.length; i++) {
+    let sharingItemMarker = new kakao.maps.Marker({
+      position: new kakao.maps.LatLng(
+        sharingLists[i]?.latitude,
+        sharingLists[i]?.longitude
+      ),
+      clickable: true,
+    });
+    markers.push(sharingItemMarker);
+
+    const infoWindowContent = `<div class="bg-white rounded-2xl">
+    <div>${sharingLists[i]?.title}</div>
+    </div>`;
+    let infoWindow = new kakao.maps.InfoWindow({
+      content: infoWindowContent,
+      position: sharingItemMarker.getPosition(),
+      removable: true,
+    });
+    kakao.maps.event.addListener(sharingItemMarker, 'click', function () {
+      infoWindow.open(map, sharingItemMarker);
+    });
+  }
+  clusterer.addMarkers(markers);
 };
