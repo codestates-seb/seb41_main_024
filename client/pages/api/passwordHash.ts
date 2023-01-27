e011b50d0cbbb4a06c7877b9e99363ac9d4a9213
koreadinosaur <khj7741@gmail.com>
2023-01-27 16:07:03 +0900

    feat - #264 비밀번호 해시 서버 추가

--- client/pages/api/passwordHash.ts
+++ client/pages/api/passwordHash.ts
@@ -1,13 +1,15 @@
 import bcrypt from 'bcrypt';
-export default async function hashPassword(req, res) {
+import { NextApiRequest, NextApiResponse } from 'next';
+export default async function hashPassword(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const saltRounds = 10;
-  console.log(req.body);
   const { pw } = req.body;
 
-  const result = await bcrypt.hash(pw, saltRounds, function (err, hash) {
+  const result = bcrypt.hash(pw, saltRounds, function (err, hash) {
     // Store hash in your password DB.
-    console.log('in func : ', hash);
 
-    return res.status(201).json({ data: hash });
+    return res.status(201).json({ hashedPassword: hash });
   });
 }
