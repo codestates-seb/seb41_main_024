7fecb85739092f079659343e8086dc9920d8ac2a
koreadinosaur <khj7741@gmail.com>
2023-01-28 03:27:23 +0900

    style - #287 no content 컴포넌트 margin삭제

--- client/components/molecules/noContent/NoContent.tsx
+++ client/components/molecules/noContent/NoContent.tsx
@@ -2,7 +2,7 @@ import { ReactComponent as Logo } from '../../../public/logos/logoNoContent.svg'
 
 const NoContent = () => {
   return (
-    <div className="mt-[30rem]">
+    <div>
       <Logo width="240" height="148" />
     </div>
   );
