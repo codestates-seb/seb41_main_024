037ba6d25f1fddd8b6a7e8f0d7088c6c0e3744d1
SHWsgithub <shw961016@gmail.com>
2023-01-29 02:15:44 +0900

    refacoter: 모집완료 메세지 CSS  수정

--- client/components/molecules/chatNoticeRow/ChatNoticeRow.tsx
+++ client/components/molecules/chatNoticeRow/ChatNoticeRow.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { chatNoticeRowType } from './chatNoticeRowType';
 
+const CLASS_STRING = "py-[0.5rem] px-[1.25rem] text-[#fff] font-normal leading-4"
+
 const ChatNoticeRow = ({message}: chatNoticeRowType ) => {
-  const CLASS_STRING = "inline-block py-[0.5rem] px-[1.25rem] bg-[rgba(217,217,217,0.3)] text-[#fff] font-normal leading-4 rounded"
   return (
     <div className="flex justify-center my-9 mx-0">
         {typeof message === 'string' && (
-          <strong className={CLASS_STRING}>
+          <strong className="inline-block py-[0.5rem] px-[1.25rem] bg-[rgba(217,217,217,0.3)] text-[#fff] font-normal leading-4 rounded">
             {message}
           </strong>
         )}
         {typeof message === 'object' &&
-          <div className='flex flex-col text-center'>
+          <div className='flex flex-col text-center bg-[rgba(217,217,217,0.3)]'>
             <strong className={CLASS_STRING}>{message[0]}</strong>
             <strong className={CLASS_STRING}>{message[1]}</strong>
             <strong className={CLASS_STRING}>{message[2]}</strong>
