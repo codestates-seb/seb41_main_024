fe157ee2ca54e7137e616f1e4601994f46762e7b
kimeunsoo <noah.eunsoo0504@gmail.com>
2023-01-28 22:11:55 +0900

    feat - #228 : 마이페이지 - 로그인 체커 추가

--- client/pages/mypage/index.tsx
+++ client/pages/mypage/index.tsx
@@ -4,22 +4,25 @@ import UserInfoEdit from '../../components/container/userInfoEdit/UserInfoEdit';
 import MyAllSharing from '../../components/container/myAllSharing/MyAllSharing';
 import NTabs from '../../components/organisms/nTabs/NTabs';
 import MyInquiry from '../../components/container/myInquiry/MyInquiry';
+import LoginChecker from '../../components/container/loginChecker/LoginChecker';
 
 const Mypage = () => {
   return (
-    <div className="flex flex-col">
-      <div className="flex justify-center items-end h-[4.5rem] mb-10">
-        <p className="text-xl">마이페이지</p>
+    <LoginChecker path="/login">
+      <div className="flex flex-col">
+        <div className="flex justify-center items-end h-[4.5rem] mb-10">
+          <p className="text-xl">마이페이지</p>
+        </div>
+        <NTabs
+          ariaLabel="내 서비스 탭"
+          tabLabels={['내정보', '쉐어링', '1:1문의']}
+        >
+          <UserInfoEdit />
+          <MyAllSharing />
+          <MyInquiry />
+        </NTabs>
       </div>
-      <NTabs
-        ariaLabel="내 서비스 탭"
-        tabLabels={['내정보', '쉐어링', '1:1문의']}
-      >
-        <UserInfoEdit />
-        <MyAllSharing />
-        <MyInquiry />
-      </NTabs>
-    </div>
+    </LoginChecker>
   );
 };
 
