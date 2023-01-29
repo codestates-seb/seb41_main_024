57c0e285121586aa41e3b35edf5bf442770f0cb7
SHWsgithub <shw961016@gmail.com>
2023-01-29 19:56:11 +0900

    refactor: 채팅방 나가기 이후 바로 새로운 채팅리스트를 받을 수 있도록 수정

--- client/pages/chatlist/index.tsx
+++ client/pages/chatlist/index.tsx
@@ -1,18 +1,18 @@
 import ChatItem from '../../components/organisms/chatItem/ChatItem';
 import ProductImg from '../../public/chatItem/productImg.svg';
-import ProductImg02 from '../../public/chatItem/productImg02.svg';
-import ProductImg03 from '../../public/chatItem/productImg03.svg';
-import ProductImg04 from '../../public/chatItem/productImg04.svg';
-import ProductImg05 from '../../public/chatItem/productImg05.svg';
 import Link from 'next/link';
 
-import { useRouter } from 'next/router';
-import { useQuery, useMutation } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 import { getMySharing } from '../../api/mySharing';
+import { useEffect } from 'react';
 
 const ChatList = () => {
-  const { data } = useQuery(['mySharing'], getMySharing);
+  const { data,refetch } = useQuery(['mySharing'], getMySharing);
   const chatListData = data?.data.data;
+
+  useEffect(() => {
+    refetch();
+  }, [])
 
   console.log(chatListData);
   return (
