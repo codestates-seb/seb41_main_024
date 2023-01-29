d72e81c7cdd6f80b5c8052d7be2252508534b3cb
parkjiyun98 <dysm0084@gmail.com>
2023-01-30 00:09:59 +0900

    feat #312 - refresh token을 DB에 저장

---
+++ server/src/main/java/com/main024/ngether/auth/RefreshToken/RefreshTokenRepository.java
@@ -0,0 +1,7 @@
+package com.main024.ngether.auth.RefreshToken;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken,Long> {
+
+}
