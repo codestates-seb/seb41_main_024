c960362202cabc784b0ee549b8365a933df994fd
koreadinosaur <khj7741@gmail.com>
2023-01-29 21:19:03 +0900

    feat - #308 기존 로직 삭제

--- client/pages/nearby/index.tsx
+++ client/pages/nearby/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { dehydrate, QueryClient, useQuery } from '@tanstack/react-query';
 import {
+  getAllSharingPosts,
   getPostsInSpecifiedLocation,
   searchPostsByTitle,
 } from '../../api/post';
@@ -11,77 +12,38 @@ import TabPanel from '../../components/atoms/tabPanel/TabPanel';
 import NearByList from '../../components/organisms/nearByList/NearByList';
 import { ListItemPropsType } from '../../components/molecules/sharingListItem/sharingListItemType';
 import ToggleButtons from '../../components/molecules/toggleButtonGroup/ToggleButtonGroup';
-
+const TOGGLE_VALUES = [
+  { value: 0.5, label: '0.5Km' },
+  { value: 1, label: '1Km' },
+  { value: 1.5, label: '1.5Km' },
+];
 interface nearbyPropsType {
   dehydratedState: any;
   lat: number;
   lng: number;
   argumentOfLocation: useSearchPropsType['argumentOfLocation'];
   searchOption: string;
+  address: string;
 }
 const LABEL = ['거리순', '최신순'];
-const Index = ({
-  dehydratedState,
-  lat,
-  lng,
-  argumentOfLocation,
-  searchOption,
-}: nearbyPropsType) => {
-  const [sharingLists, setSharingLists] = useState(
-    dehydratedState?.queries[0]?.state.data.data
-  );
-  const [sharingListsSortedByTime, setSharingListsSortedByTime] = useState<
-    ListItemPropsType[]
-  >([]);
+const Index = ({ dehydratedState, lat, lng, address }: nearbyPropsType) => {
+  console.log(dehydratedState);
+
   const [mapCenter, setMapCenter] = useState({
     lat,
     lng,
-    address: argumentOfLocation?.locationData?.address,
+    address: address,
   });
   const [alignment, setAlignment] = useState<number>(1.5);
 
   useEffect(() => {
-    setMarkerCluster(mapCenter, sharingLists, setMapCenter);
-  }, [sharingLists, dehydratedState?.queries[0]?.state.data.data]);
+    const sharingListsInMap = dehydratedState?.queries[0]?.state.data.data;
+    setMarkerCluster(mapCenter, sharingListsInMap, setMapCenter);
+  }, []);
 
-  const { data, refetch } = useQuery({
-    queryKey: ['sharingList'],
-    queryFn: () => {
-      return getPostsInSpecifiedLocation({
-        locationData: mapCenter,
-        range: alignment || 1.5,
-        category: argumentOfLocation?.category || 'product',
-        page: 1,
-        size: 300,
-      });
-    },
-    onSuccess: (data) => {
-      setSharingLists(data.data);
-    },
-    enabled: false,
-    retry: false,
-  });
-
-  useEffect(() => {
-    if (searchOption !== '글 제목') {
-      refetch();
-    }
-  }, [mapCenter.address, alignment]);
-  const [locationError, setLocationError] = useState('');
   const [currentTab, setCurrentTab] = useState(0);
 
   const handleChange = (event: React.SyntheticEvent, newCurrentTab: number) => {
-    if (newCurrentTab === 1) {
-      const sortedByTime: ListItemPropsType[] =
-        sharingLists?.length > 0
-          ? [...sharingLists]?.sort(
-              (a, b) =>
-                new Date(b.createDate).valueOf() -
-                new Date(a.createDate).valueOf()
-            )
-          : [];
-      setSharingListsSortedByTime(sortedByTime);
-    }
     setCurrentTab(newCurrentTab);
   };
 
@@ -91,11 +53,7 @@ const Index = ({
   ) => {
     setAlignment(newAlignment);
   };
-  const TOGGLE_VALUES = [
-    { value: 0.5, label: '0.5Km' },
-    { value: 1, label: '1Km' },
-    { value: 1.5, label: '1.5Km' },
-  ];
+
   return (
     <div className="flex flex-col items-center">
       <div className="mx-auto w-full h-fit">
@@ -111,7 +69,6 @@ const Index = ({
             있습니다
           </em>
         </p>
-        {locationError && <div>{locationError}</div>}
       </div>
       <BasicTabs
         currentTab={currentTab}
@@ -122,7 +79,7 @@ const Index = ({
       <div className="flex w-[100%] items-center">
         <p>
           <strong className="font-[500] whitespace-nowrap">
-            주변 게시물 수 : {sharingLists?.length}
+            주변 게시물 수 :
           </strong>
         </p>
         <div className="flex w-[100%] items-center justify-end mt-4 mr-4">
@@ -135,10 +92,10 @@ const Index = ({
         </div>
       </div>
       <TabPanel currentTab={currentTab} index={0}>
-        <NearByList sharingLists={sharingLists} />
+        {/* <NearByList sharingLists={[]} /> */}
       </TabPanel>
       <TabPanel currentTab={currentTab} index={1}>
-        <NearByList sharingLists={sharingListsSortedByTime || []} />
+        {/* <NearByList sharingLists={[]} /> */}
       </TabPanel>
     </div>
   );
@@ -146,58 +103,24 @@ const Index = ({
 export default Index;
 
 export async function getServerSideProps(context: any) {
-  const {
-    range: defaultRange,
-    category: defaultCategory,
-    page,
-    size,
-    lat,
-    lng,
-    address,
-    searchOption,
-    type,
-    keyword,
-  } = context?.query;
-  console.log(context?.query);
+  const { lat, lng, address } = context?.query;
 
   const requestData = {
     lat: Number(lat),
     lng: Number(lng),
     address: decodeURIComponent(address),
   };
-
-  const argumentOfLocation = {
-    locationData: requestData,
-    range: defaultRange || '',
-    category: defaultCategory || '',
-    page: page || 1,
-    size: size || 300,
-  };
-  const argumentOfTitle = {
-    type: type || 4,
-    keyword: decodeURIComponent(keyword),
-    page: page || 1,
-    size: size || 300,
-  };
+  console.log(requestData);
 
   const queryClient = new QueryClient();
-  await queryClient.prefetchQuery(['sharingList'], async () => {
-    if (searchOption === '주소') {
-      return await getPostsInSpecifiedLocation(argumentOfLocation);
-    } else if (searchOption === '글 제목') {
-      return await searchPostsByTitle(argumentOfTitle);
-    }
-  });
-  const sharingLists = dehydrate(queryClient);
+  await queryClient.prefetchQuery(['sharingListInMap'], getAllSharingPosts);
 
   return {
     props: {
       dehydratedState: dehydrate(queryClient) || '',
-      lat: lat || 37.4954330863648,
-      lng: lng || 126.88750531451,
-      argumentOfLocation: argumentOfLocation || '',
-      argumentOfTitle: argumentOfTitle || '',
-      searchOption: searchOption || '',
+      lat: requestData.lat || 37.4954330863648,
+      lng: requestData.lng || 126.88750531451,
+      address: requestData.address || '',
     },
   };
 }
