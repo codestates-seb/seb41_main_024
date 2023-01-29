d50df9f7ac54fc62c6ed37ed7fb3f496c3ec67b0
koreadinosaur <khj7741@gmail.com>
2023-01-29 17:36:15 +0900

    feat - #298 모달 추가

--- client/components/molecules/detailBottom/DetailBottom.tsx
+++ client/components/molecules/detailBottom/DetailBottom.tsx
@@ -15,10 +15,16 @@ const DetailBottom = ({
   handleLike,
   handleReport,
   handleGether,
-  modalOpen,
-  handleModalOpen,
-  handleClose,
+  isReportModalOpen,
+  handleReportModalOpen,
+  handleReportModalClose,
   handleComplete,
+  isGetherModalOpen,
+  handleGetherModalOpen,
+  handleGetherModalClose,
+  isCompleteModalOpen,
+  handleIsCompleteModalOpen,
+  handleIsCompleteModalClose,
 }: DetailBottomPropsType) => {
   return (
     <div className="flex justify-between items-center p-4 mb-4 border-y-1 px-2 py-4 border-x-0 border-solid border-[#475569]">
@@ -42,8 +48,8 @@ const DetailBottom = ({
       </Button>
       <div>
         <ModalComponent
-          modalOpen={modalOpen}
-          handleClose={handleClose}
+          modalOpen={isReportModalOpen}
+          handleClose={handleReportModalClose}
           title="해당 게시물을 신고하시겠습니까? 신고 즉시 게시물이 노출되지 않습니다."
           onClick={handleReport}
           positiveResponse="예 신고하겠습니다"
@@ -52,8 +58,8 @@ const DetailBottom = ({
           negativeColor={'black'}
         />
         <ModalComponent
-          modalOpen={modalOpen}
-          handleClose={handleClose}
+          modalOpen={isGetherModalOpen}
+          handleClose={handleGetherModalClose}
           title="공동 구매에 참여하시면 공동구매 채팅방으로 이동됩니다. 이웃들과 함께 세부사항을 논의해보세요! "
           onClick={handleGether}
           positiveResponse="예 참여하겠습니다"
@@ -61,9 +67,23 @@ const DetailBottom = ({
           negativeResponse="취소"
           negativeColor={'red'}
         />
+        <ModalComponent
+          modalOpen={isCompleteModalOpen}
+          handleClose={handleIsCompleteModalClose}
+          title="모집을 마감하시겠습니까? 모집을 마감하면 더 이상 다른 유저들이 참여할 수 없습니다."
+          onClick={handleComplete}
+          positiveResponse="예 마감하겠습니다"
+          positiveColor={'#63A8DA'}
+          negativeResponse="취소"
+          negativeColor={'red'}
+        />
 
         {isOpen && !isWriter && (
-          <Button onClick={handleModalOpen} variant="contained" className="m-2">
+          <Button
+            onClick={handleGetherModalOpen}
+            variant="contained"
+            className="m-2"
+          >
             참여하기
           </Button>
         )}
@@ -74,7 +94,7 @@ const DetailBottom = ({
         )}
         {!isWriter && (
           <Button
-            onClick={handleModalOpen}
+            onClick={handleReportModalOpen}
             variant="contained"
             className="bg-[red] m-2"
           >
@@ -82,7 +102,11 @@ const DetailBottom = ({
           </Button>
         )}
         {isWriter && isOpen && (
-          <Button onClick={handleComplete} variant="contained" className="m-2">
+          <Button
+            onClick={handleIsCompleteModalOpen}
+            variant="contained"
+            className="m-2"
+          >
             모집 마감하기
           </Button>
         )}
