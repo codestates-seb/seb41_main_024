746824cb9a75c6e8a6f27bf6b5f0e3ee0ad235dc
kimeunsoo <noah.eunsoo0504@gmail.com>
2023-01-30 10:03:56 +0900

    feat - #319 : 회원가입, 마이페이지 수정 - 이메일, 닉네임, 핸드폰 번호 중복 검사 추가

--- client/components/organisms/loading/Loading.tsx
+++ client/components/organisms/loading/Loading.tsx
@@ -1,6 +1,6 @@
 const Loading = () => {
   return (
-    <div className="flex fixed inset-0 justify-center items-center z-[100] bg-black bg-opacity-[0.5] text-white text-[2rem]">
+    <div className="flex fixed top-[3.125rem] bottom-0 right-0 left-0 justify-center items-center z-[100] bg-black bg-opacity-[0.5] text-white text-[2rem]">
       <strong>Loading...</strong>
     </div>
   );
