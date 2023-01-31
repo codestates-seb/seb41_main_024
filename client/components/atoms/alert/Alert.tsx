e8eb2cd347ae26b7d66e8b71e03104bd1ca63277
Park Kyung Hyun <pikadev1771@gmail.com>
2023-01-31 19:33:06 +0900

    refactor - badge color 변경, 배송비 포함 문구 삭제

--- client/components/atoms/alert/Alert.tsx
+++ client/components/atoms/alert/Alert.tsx
@@ -5,7 +5,15 @@ import Badge from '@mui/material/Badge';
 const Alert = ({ unreadCount }: alertType) => {
   return (
     <div className="flex justify-center items-center w-6 h-6">
-      <Badge badgeContent={unreadCount} color="primary"></Badge>
+      <Badge
+        badgeContent={unreadCount}
+        sx={{
+          '& .MuiBadge-badge': {
+            color: 'white',
+            backgroundColor: '#ff5656',
+          },
+        }}
+      ></Badge>
     </div>
   );
 };
--- client/components/molecules/postMeta/PostMeta.tsx
+++ client/components/molecules/postMeta/PostMeta.tsx
@@ -31,7 +31,7 @@ const PostMeta = ({ productData, isLiked, handleLike }: PostMetaType) => {
               <ElapsedTime createDate={productData?.createDate} />
             </p>
             <strong className="text-lg font-medium">
-              {productData?.price?.toLocaleString()}원 (배송비포함)
+              {productData?.price?.toLocaleString()}원
             </strong>
           </Stack>
           <Stack>
@@ -96,37 +96,6 @@ const PostMeta = ({ productData, isLiked, handleLike }: PostMetaType) => {
         </Box>
       </Stack>
     </Box>
-
-    // <div className="ml-4 mt-6">
-    //   <div className="mb-2.5">
-    //     <strong className="text-xl font-medium">{productData?.title}</strong>
-    //   </div>
-    //   <p className="text-sm text-[#475569] mb-4">
-    //     {productData?.category}•
-    //     <ElapsedTime createdAt={productData?.createDate} />
-    //   </p>
-    //   <div className=" mb-2.5">
-    //     <strong className="text-lg font-medium">
-    //       {productData?.price.toLocaleString()}원(배송비포함)
-    //     </strong>
-    //   </div>
-    //   <p className="text-sm text-[#475569] mb-4">
-    //     모집기간: ~{productData?.deadLine}
-    //   </p>
-    //   <p className="text-sm text-[#475569] mb-4">
-    //     현재 모집인원: {productData?.curNum}명 / 총 모집인원:
-    //     {productData?.maxNum}명
-    //   </p>
-    //   <p className="text-sm text-[#475569] mb-4">
-    //     참여시 예상금액:
-    //     {productData &&
-    //       (productData?.price / (productData?.curNum + 1)).toLocaleString()}
-    //     원
-    //   </p>
-    //   <p className="truncate text-sm text-[#475569] mb-4">
-    //     판매 제품 링크 : {productData?.productsLink}
-    //   </p>
-    // </div>
   );
 };
 
