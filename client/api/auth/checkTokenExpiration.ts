9c3f79672d8744419e459a60b7c5a9ca69e10e07
koreadinosaur <khj7741@gmail.com>
2023-02-01 22:00:53 +0900

    fix - #383 refresh 토큰이 있어도 return으로 함수가 종료되는 것 수정

--- client/api/auth/checkTokenExpiration.ts
+++ client/api/auth/checkTokenExpiration.ts
@@ -7,15 +7,14 @@ const accessToken = Cookies.get('access_token');
 const refreshToken = Cookies.get('refresh_token');
 
 export const checkTokenExpiration = () => {
-  // 로그인 안했으면 리턴
+  // 로그인 안했으면 리턴 accessToken === undefined && accessToken === undefined 이었던 것 수정
   if (accessToken === undefined && refreshToken === undefined) {
     return;
   }
-
   // 리프레쉬 토큰은 있지만 엑세스 토큰이 expires 옵션에 의해 만료됐다고 예상되는 경우
   if (accessToken === undefined && refreshToken) {
     // 헤더에 리프레쉬 토큰을 담아 api/reissue로 get req
-    axios
+    return axios
       .get(`${REQUEST_URL}/api/reissue`, { headers: { refresh: refreshToken } })
       .then((res) => {
         // res header에 authorization을 다시 엑세스 토큰 쿠키에 set
