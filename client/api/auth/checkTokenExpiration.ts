b731e326d1d6e3cd0bc59ec0e6b0d785b75da3eb
HyeongJin Kim <khj7741@gmail.com>
2023-02-01 19:19:43 +0900

    fix - 조건문 수정

--- client/api/auth/checkTokenExpiration.ts
+++ client/api/auth/checkTokenExpiration.ts
@@ -8,23 +8,27 @@ const refreshToken = Cookies.get('refresh_token');
 
 export const checkTokenExpiration = () => {
   // 로그인 안했으면 리턴
-  if(accessToken === undefined && accessToken === undefined) {
+  if (accessToken === undefined && refreshToken === undefined) {
     return;
   }
+
   // 리프레쉬 토큰은 있지만 엑세스 토큰이 expires 옵션에 의해 만료됐다고 예상되는 경우
-  if(accessToken === undefined && refreshToken) {
-  // 헤더에 리프레쉬 토큰을 담아 api/reissue로 get req
-    axios.get(`${REQUEST_URL}/api/reissue`, { headers : {refresh: refreshToken} })
-    .then(res => {
-      // res header에 authorization을 다시 엑세스 토큰 쿠키에 set
-      res.headers.authorization &&
-      Cookies.set('access_token', res.headers.authorization);
-      // refresh 토큰 만료 예상시 res header에 refresh 토큰도 같이 오기 때문에, 만약 헤더에 있다면 같이 set
-      res.headers.refresh &&
-      Cookies.set('refresh', res.headers.refresh);
-    })
-    .catch(err => {
-      console.error(err);
-    });
+  if (accessToken === undefined && refreshToken) {
+    // 헤더에 리프레쉬 토큰을 담아 api/reissue로 get req
+    axios
+      .get(`${REQUEST_URL}/api/reissue`, { headers: { refresh: refreshToken } })
+      .then((res) => {
+        // res header에 authorization을 다시 엑세스 토큰 쿠키에 set
+        res.headers.authorization &&
+          Cookies.set('access_token', res.headers.authorization, {
+            expires: 0.079,
+          });
+        // refresh 토큰 만료 예상시 res header에 refresh 토큰도 같이 오기 때문에, 만약 헤더에 있다면 같이 set
+        res.headers.refresh &&
+          Cookies.set('refresh', res.headers.refresh, { expires: 20 });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   }
-}
+};
